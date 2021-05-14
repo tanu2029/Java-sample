71a24d49f01c94b40be362aa18b760f4fac4dd5e
Tanmay Jadhav <tanmay2jadhav@gmail.com>
2021-05-14 10:59:46 +0530

    my first code

---
+++ src/sample_package/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sample_package;
+
+/**
+ * @author tanmay
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("HelloWorld");
+
+	}
+
+}
